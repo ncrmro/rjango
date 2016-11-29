ff3f32f335784054b17e25a31b753b5eece206fb
Nicholas Romero <ncrmro@gmail.com>
2016-11-28 23:34:58 -0600

    Changes wsgi.py to use prod settigns

--- src/ango/wsgi.py
+++ src/ango/wsgi.py
@@ -13,7 +13,7 @@ from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ango.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ango.settings.prod")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
