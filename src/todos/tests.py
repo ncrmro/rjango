b76c8e01f9e33bc9944241e95bb660d21cf11d6a
Nicholas Romero <ncrmro@gmail.com>
2016-12-12 10:08:22 -0600

    Adds sample test for all todos graphql query

--- src/todos/tests.py
+++ src/todos/tests.py
@@ -1,7 +1,7 @@
 import json
+
 from django.contrib.auth.models import User
-from django.test import TestCase, RequestFactory
-from django.http import JsonResponse
+from django.test import TestCase, Client
 
 from .models import TodoModel
 
@@ -22,3 +22,27 @@ class TodoTest(TestCase):
     def test_todo_created_was_created(self):
         todo = TodoModel.objects.get(text="My todo")
         self.assertEqual(todo.text, 'My todo')
+
+    def test_all_todos_graphql_endpoint(self):
+        """Test all_todos graphql query"""
+        query = {
+            "query": "query {allTodos(first: 10){edges{node{id,text}}}}"
+        }
+        expected_response = {
+            "data": {
+                "allTodos": {
+                    "edges": [
+                        {
+                            "node": {
+                                "id": "VG9kb05vZGU6MQ==",
+                                "text": "My todo"
+                            }}
+                    ]}
+            }
+        }
+        c = Client()
+        response = c.post('/graphql', query)
+        # Decode byte code response.content and load the json
+        graphql_response = json.loads(response.content.decode('ascii'))
+        print(graphql_response)
+        self.assertEqual(graphql_response, expected_response)
