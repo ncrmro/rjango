5516386897f38e84e54d8f9dc44127d105a3f897
Nicholas Romero <ncrmro@gmail.com>
2017-01-19 18:04:04 -0600

    Adds example todo mutation

--- bend/src/todos/schema.py
+++ bend/src/todos/schema.py
@@ -1,4 +1,4 @@
-from graphene import AbstractType, Node
+from graphene import AbstractType, Node, Field, String, relay, ObjectType, Int
 from graphene_django.filter import DjangoFilterConnectionField
 from graphene_django.types import DjangoObjectType
 
@@ -11,6 +11,25 @@ class TodoNode(DjangoObjectType):
         interfaces = (Node,)
 
 
+class CreateTodo(relay.ClientIDMutation):
+    class Input:
+        user_id = Int(required=True)
+        text = String(required=True)
+
+    todo = Field(TodoNode)
+
+    @classmethod
+    def mutate_and_get_payload(cls, input, context, info):
+        user_id = input.get('user_id')
+        text = input.get('text')
+        todo = TodoModel.objects.create(user_id=user_id, text=text)
+        return CreateTodo(todo=todo)
+
+
 class Query(AbstractType):
     todo = Node.Field(TodoNode)
     all_todos = DjangoFilterConnectionField(TodoNode)
+
+
+class Mutation(AbstractType):
+    create_todo = CreateTodo.Field()
