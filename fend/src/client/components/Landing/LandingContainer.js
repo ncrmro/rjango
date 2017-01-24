e7574f8f1d8055d75e23e7091f12e87bccd77bf8
Nicholas Romero <ncrmro@gmail.com>
2017-01-24 15:12:33 -0600

    Checks in missing landing container and updates circle.yml with collectstatic command

---
+++ fend/src/client/components/Landing/LandingContainer.js
@@ -0,0 +1,12 @@
+import Relay from "react-relay";
+import Landing from "./LandingComponent";
+
+export default Relay.createContainer(Landing, {
+    fragments: {
+        viewer: () => Relay.QL`
+            fragment on UserNode {
+                username,
+                email
+            }`
+    }
+});
