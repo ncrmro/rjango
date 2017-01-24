f8d002115304f225f88387d35085bbdc41b6bc6b
Nicholas Romero <ncrmro@gmail.com>
2017-01-24 12:52:16 -0600

    CSRF exempt graphql endpoint, updates sample test and cleans up settings

--- bend/src/ango/tests.py
+++ bend/src/ango/tests.py
@@ -11,11 +11,10 @@ class HomePageTest(LiveServerTestCase):
         self.selenium.quit()
         super(HomePageTest, self).tearDown()
 
-    def test_register(self):
+    def test_home_page(self):
         selenium = self.selenium
         selenium.implicitly_wait(10)  # seconds
 
         # Opening the link we want to test
-        selenium.get('http://localhost:8000')
-
+        selenium.get(self.live_server_url)
         assert 'Relay Fullstack' in selenium.page_source
