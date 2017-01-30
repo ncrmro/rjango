b44474c54cc3b3eab7e801e15a2a7c33ef5af937
Nicholas Romero <ncrmro@gmail.com>
2017-01-30 16:01:25 -0600

    Updates wsgi to use the apps folder

--- src/ango/wsgi.py
+++ src/ango/wsgi.py
@@ -7,11 +7,14 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
+# Add the modules folder
+PROJECT_ROOT = os.path.dirname(__file__)
+sys.path.insert(0, os.path.join(PROJECT_ROOT, '..', 'apps'))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ango.settings.prod")
 
