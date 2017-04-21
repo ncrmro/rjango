e0ef00960dc2acdcfb14485c8a42f4d1cdec6431
Nicholas Romero <ncrmro@gmail.com>
2017-04-21 08:37:03 -0500

    Textfield handles key presses now

--- client/components/Textfield/Textfield.js
+++ client/components/Textfield/Textfield.js
@@ -1,7 +1,6 @@
 // @flow
 import React from 'react';
 import { Textfield as MdlTextField } from 'react-mdl';
-
 export default class Textfield extends React.Component {
   constructor(props:Object) {
     super(props);
@@ -10,16 +9,26 @@ export default class Textfield extends React.Component {
     };
   }
 
-  setFieldValue(e) {
-    const value = e.target.value;
+  setFieldValue(event) {
+    const value = event.target.value;
     this.setState({ value: value });
   }
+
+  handleKeyPress = (event) => {
+    const value = event.target.value;
+    const action = this.props.action;
+    const todo = {text: value};
+    if (event.key == 'Enter') {
+      action(todo);
+    }
+  };
 
   render() {
     const { label, floatingLabel } = this.props;
     return (
       <MdlTextField
         onChange={this.setFieldValue.bind(this)}
+        onKeyPress={this.handleKeyPress}
         label={label}
         value={this.state.value}
         floatingLabel={floatingLabel}
