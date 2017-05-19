74804e03aa4d199e12117358923f9d009ce941fe
Nicholas Romero <ncrmro@gmail.com>
2017-05-19 10:46:41 -0500

    Nav working

--- client/components/App/AppComponent.js
+++ client/components/App/AppComponent.js
@@ -2,14 +2,13 @@ import React from 'react';
 import 'normalize.css/normalize.css';
 import 'react-mdl/extra/css/material.cyan-red.min.css';
 
-//import Navbar from '../Nav/Nav';
+import Navbar from '../Nav/Nav';
 import Footer from '../Footer/FooterComponent';
 import styles from './App.scss';
-import isAuthenticatedComponent from '../../modules/users/isAuthenticated/isAuthenticatedComponent';
 
 const App = ({ isAuthenticated, router, children }) =>
   <div className={styles.root} >
-
+    <Navbar isAuthenticated />
     <div className={styles.navBackground} />
     <div className={styles.content} >
       {children}
