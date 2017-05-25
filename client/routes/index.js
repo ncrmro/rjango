0fba7da0c24430c7370a9493f59f64b787edcae0
Nicholas Romero <ncrmro@gmail.com>
2017-05-25 14:32:43 -0500

    Adds relay with queries example

--- client/routes/index.js
+++ client/routes/index.js
@@ -1,6 +1,8 @@
 import App from '../components/App/App';
 import Landing from '../components/Landing/LandingComponent';
 import Signup from '../modules/users/Signup/SignupComponent'
+import Todos from '../modules/users/Todos/Todos'
+
 export const authRoutes = [
   {
     path: '/signup',
@@ -12,6 +14,14 @@ export const authRoutes = [
   }
 ];
 
+export const todoRoutes = [
+  {
+    path: '/todos',
+    component: Todos,
+    queries: 'queries'
+  }
+];
+
 export const routes = [
   { 
     component: App,
@@ -20,7 +30,8 @@ export const routes = [
       queries: 'queries'
     },
     childRoutes: [
-      ...authRoutes
+      ...authRoutes,
+      ...todoRoutes
     ]
   }
 ];
