f6523b032bef3cd4fa5beb372e16eb45d26d710a
Nicholas Romero <ncrmro@gmail.com>
2017-06-03 09:39:10 -0500

    Passing eslint relay and route utils

--- client/utils/RouteUtil.js
+++ client/utils/RouteUtil.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 import React from 'react';
 import { Route } from 'react-router-dom';
 
--- client/utils/relay.js
+++ client/utils/relay.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import RelayLookupQueryRenderer from './RelayLookupQueryRenderer';
+import Loading from '../components/Loading/Loading';
 
 const {
   Environment,
@@ -6,16 +8,13 @@ const {
   RecordSource,
   Store,
 } = require('relay-runtime');
-import { QueryRenderer, graphql } from 'react-relay';
-import RelayLookupQueryRenderer from './RelayLookupQueryRenderer';
-import Loading from '../components/Loading/Loading';
 
 const source = new RecordSource();
 const store = new Store(source);
 
 // Define a function that fetches the results of an operation (query/mutation/etc)
 // and returns its results as a Promise:
-function fetchQuery(operation, variables, cacheConfig, uploadables) {
+function fetchQuery(operation, variables/* , cacheConfig, uploadables*/) {
   // Caching and relay records merge here
   // console.log(operation, variables);
   // console.log(store._recordSource._records);
@@ -43,17 +42,24 @@ export const environment = new Environment({
   store,
 });
 
+const RelayChild = (passedProps, error, props) => {
+  if (props) {
+    return (<passedProps.ChildComponent
+      {...props}
+      routes={passedProps.routes}
+      environment={environment}
+    />);
+  } else if (error) {
+    return <div>Error: {error}</div>;
+  }
+  return <Loading />;
+};
+
 export const RelayComponent = passedProps =>
   <RelayLookupQueryRenderer
     lookup
     environment={environment}
     query={passedProps.query}
     variables={passedProps.variables}
-    render={({ error, props }) => props ?
-      <passedProps.ChildComponent
-        {...props}
-        routes={passedProps.routes}
-        environment={environment}
-      />
-      : <Loading />}
+    render={({ error, props }) => RelayChild(passedProps, error, props)}
   />;
