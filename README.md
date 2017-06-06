70706c067b199e8005cdd6c1de2b8537e6676581
Nicholas Romero <ncrmro@gmail.com>
2017-06-06 11:38:48 -0500

    Clean up and simplify project

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # reango
-The Django + GraphQL Relay backend.
+The Django + GraphQL Relay Modern backend.
 
 
 ## Features
@@ -19,38 +19,7 @@ yarn
 
 ## Prod
 
-### Docker
-Sample docker-compose.yml and dockerfile are enough to test out the nginx/database/staticfiles
-
-Base image is alpine and after dependencies and staticfiles weighs in at 130.5mb
-
-docker run -it --entrypoint=/bin/bash ncrmro/reango
-docker exec -i -t reango bash
-
-#### Docker Compose
-```
-docker-compose build
-docker-compose up
-docker-compose run reango manage.py migrate
-docker-compose run reango manage.py createsuperuser
-```
-
-### Heroku
-A fresh dyno will need the following ran
-
-Clear existing buildpacks and set the python and nodejs buildpacks. This will handle installing pip and npm installs. Npm host post install generate grapqhl schema and build frontend.
-`heroku buildpacks:clear`
-`heroku buildpacks:set heroku/python`
-`heroku buildpacks:set heroku/nodejs`
-Generate a secrete key and set allowed host if need be.
-`python -c 'import random; import string; print("".join([random.SystemRandom().choice(string.digits + string.ascii_letters + string.punctuation) for i in range(100)]))'`
-`heroku config:set ALLOWED_HOSTS=['*'] DJANGO_SETTINGS_MODULE='reango.settings.prod' SECRET_KEY=_generate_key`
-Get logs
-`heroku logs --app reango-dev`
-Run manage.py commands
-`heroku run --app APP bash`
-`python manage.py migrate`
-`python manage.py createsuperuser`
+See readme in `./lib/deployment`
 
 ### Front-End originally based on the awesome Relay Fullstack
 https://github.com/lvarayut/relay-fullstack
--- server/users/tests/browser.py
+++ server/users/tests/browser.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 
 from tests.utils import SetBrowserTests, wait_for_element, \
-    css_selector, create_test_user
+    css_selector, create_test_user, login_selenium_user,id_selector
 
 class CreateUserTest(SetBrowserTests):
     def setUp(self):
@@ -20,7 +20,7 @@ class CreateUserTest(SetBrowserTests):
         # Opening the link we want to test
         selenium.get(self.live_server_url)
 
-        css_selector(selenium, '.button_sign-up-link').click()
+        css_selector(selenium, '.button_signup-link').click()
 
         # Find Fields
         email_field = wait_for_element(selenium, EC.element_to_be_clickable(
@@ -47,14 +47,12 @@ class CreateUserTest(SetBrowserTests):
         # Opening the link we want to test
         selenium.get(self.live_server_url)
 
-        css_selector(selenium, '.button_sign-up-link').click()
+        css_selector(selenium, '.button_signup-link').click()
 
         # Find Fields
-        email_field = wait_for_element(selenium, EC.element_to_be_clickable(
-                (By.ID, 'textfield-Email')))
-        password = selenium.find_element_by_id("textfield-Password")
-        password_confirmation = selenium.find_element_by_id(
-                "textfield-PasswordConfirmation")
+        email_field = id_selector(selenium, 'textfield-Email')
+        password = id_selector(selenium, 'textfield-Password')
+        password_confirmation = id_selector(selenium, 'textfield-PasswordConfirmation')
 
         # Fill out inputs and check for form valid
         email_field.send_keys("sign_up_test_user@test.com")
@@ -83,39 +81,9 @@ class LoginUserTest(SetBrowserTests):
         selenium = self.selenium
         # Opening the link we want to test
         selenium.get(self.live_server_url)
-        login_button = wait_for_element(selenium, EC.element_to_be_clickable(
-                (By.CSS_SELECTOR, '.button_login-link')))
+        login_selenium_user(self)
 
-        login_button.click()
-
-        selenium.save_screenshot('./screenshots/login_page_1.png')
-
-        # Find Fields
-        email_field = wait_for_element(selenium, EC.element_to_be_clickable(
-            (By.ID, 'textfield-Email')))
-        password = selenium.find_element_by_id("textfield-Password")
-
-        # Click Button to check for empty values and check
-        selenium.find_element_by_css_selector(
-                '.button_submit-login-form').click()
-        selenium.save_screenshot('./screenshots/login_page_2_invalid.png')
-        # assert 'Email isn\'t valid' in selenium.page_source
-        #assert 'Passwords is blank' in selenium.page_source
-
-        # Fill out inputs and check for form valid
-        email_field.send_keys("test@user.com")
-        password.send_keys("top_secret")
-        selenium.save_screenshot('./screenshots/login_page_3_now_valid.png')
-        assert 'Email isn\'t valid' not in selenium.page_source
-        assert 'Passwords is blank' not in selenium.page_source
-
-        css_selector(selenium, '.button_submit-login-form').click()
-
-        wait_for_element(selenium, EC.visibility_of_element_located(
-            (By.XPATH, "//h1[text()='Dashboard']")))
-        selenium.save_screenshot(
-            './screenshots/login_page_4_redirected_to_profile.png')
-        assert 'Dashboard' in selenium.page_source
+        assert 'This is the polls app' in selenium.page_source
 
 
 class JwtTokenExpiredTest(SetBrowserTests):
