64280ecd684b55d2c64311ff46176e88b39e9746
Nicholas Romero <ncrmro@gmail.com>
2017-06-07 10:41:21 -0500

    Make stage users command use the factory

--- server/users/management/commands/stageusers.py
+++ server/users/management/commands/stageusers.py
@@ -1,45 +1,7 @@
-import datetime
-
-from dateutil import tz
 from django.conf import settings
-from django.contrib.auth import get_user_model
 from django.core.management.base import BaseCommand
-from faker import Factory
-
-first_date = datetime.datetime(2016, 1, 1, 1, 00)
-last_date = datetime.datetime(2017, 1, 1, 1, 00)
-
-def create_test_admin(users):
-    """Create admin user if none exist"""
-    if not users:
-        get_user_model().objects.create_superuser(
-            email="admin@test.com",
-            password="test_password",
-            first_name="John",
-            last_name="Doe",
-        )
-
-fake = Factory.create()
-fake.seed(4321)
-
-def generate_fake_users(number_of_users=25):
-    """Generate 10 fake users"""
-    for _ in range(0, number_of_users):
-        fake_user = fake.profile()
-        name = fake_user['name']
-        first_name = name.split()[0]
-        last_name = name.split()[1]
-
-        join_date = fake.date_time_between_dates(datetime_start=first_date,
-                                                 datetime_end=last_date,
-                                                 tzinfo=tz.tzutc())
-
-        get_user_model().objects.get_or_create(
-            email=fake_user['mail'],
-            first_name=first_name,
-            last_name=last_name,
-            date_joined=join_date
-        )
+from users.factory import create_test_admin, generate_fake_users
+from django.contrib.auth import get_user_model
 
 class Command(BaseCommand):
     """Create admin and fake users"""
