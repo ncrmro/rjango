2d30f7496a5b6131e6d09fcdc38f01c25f74b316
ncrmro <ncrmro@gmail.com>
2017-06-19 11:25:44 -0500

    Update to latest releases

--- server/polls/factory.py
+++ server/polls/factory.py
@@ -7,8 +7,11 @@ from django.contrib.auth import get_user_model
 from .models import Choice, Vote
 from .models import Question
 
-questions = ['Do andriods dream of sleep', 'Will we make it to mars',
-             'Whose the smartest of them all']
+questions = [
+    'Do andriods dream of sheep',
+    'Will we make it to mars',
+    'Whose the smartest of them all'
+]
 pub_date = factory.LazyFunction(lambda: datetime.datetime.now(tz=pytz.UTC))
 
 
