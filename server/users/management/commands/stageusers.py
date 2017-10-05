f66fb317db09ba83980bda4f9f15163f4676f567
ncrmro <ncrmro@gmail.com>
2017-10-05 08:54:23 -0500

    Update stage users command

--- server/users/management/commands/stageusers.py
+++ server/users/management/commands/stageusers.py
@@ -1,15 +1,18 @@
-class Command(BaseCommand):
-    """Create admin and fake users"""
+from django.conf import settings
+from django.core.management.base import BaseCommand
+from users.factory import create_test_admin, generate_fake_users
+from django.contrib.auth import get_user_model
+import sys
 
-    help = 'Import Volunteers from CSV'
+class Command(BaseCommand):
 
     def add_arguments(self, parser):
-        """Amount of users to create"""
         parser.add_argument('count', type=int, default=25)
 
     def handle(self, **options):
         count = options['count']
         users = get_user_model().objects.all()
-        if settings.DEBUG:
+        # https://stackoverflow.com/questions/6957016/detect-django-testing-mode
+        if settings.DEBUG or 'test' in sys.argv:
             create_test_admin(users)
             generate_fake_users(count)
