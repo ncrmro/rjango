3903e327d54b1a0102fd867d89a288e4d7c5d62f
ncrmro <ncrmro@gmail.com>
2017-10-17 01:50:59 -0500

    Upgrade react and react router

--- client/root.js
+++ client/root.js
@@ -3,12 +3,10 @@ import BrowserRouter from 'react-router-dom/es/BrowserRouter'
 import App from 'components/App/App'
 import routes from './routes'
 import RouteWithSubRoutes from './utils/RouteUtil'
-import createBrowserHistory from 'history/es/createBrowserHistory'
 import { environment } from 'utils/relay'
 import { isAuthenticated } from 'modules/auth/utils'
 //require('offline-plugin/runtime').install()
 
-const history = createBrowserHistory()
 
 const Root = (props) =>
   <BrowserRouter history={history} >
