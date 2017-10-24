fdf87e20f79803ddc9f2392e1891b03566f5f7af
ncrmro <ncrmro@gmail.com>
2017-10-24 02:59:50 -0500

    Simplify webpack config

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,93 +1,87 @@
-'use strict';
+import path from 'path'
+import webpack from 'webpack'
+import BundleTracker from 'webpack-bundle-tracker'
+import ExtractTextPlugin from 'extract-text-webpack-plugin'
+import dotenv from 'dotenv'
 
-const path = require('path');
-const webpack = require('webpack');
-const autoprefixer = require('autoprefixer');
-const precss = require('precss');
-const BundleTracker = require('webpack-bundle-tracker');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
-const dotenv = require('dotenv');
+dotenv.load()
 
-dotenv.load();
-let appEntry;
-let devtool;
-let plugins;
-let publicPath;
-
-
-
-const stats = new BundleTracker(
+let
+  env = process.env,
+  devServerPort = env.WEBPACK_PORT ? env.WEBPACK_PORT : 3000,
+  appEntry,
+  devtool,
+  plugins,
+  publicPath,
+  buildPath = path.join(__dirname, 'static', 'bundles'),
+  statsPlugin = new BundleTracker(
     {
-        path: __dirname,
-        filename: 'static/webpack-stats.json',
-        indent: true
-    });
-
-const env = process.env;
-const devServerPort = env.WEBPACK_PORT ? env.WEBPACK_PORT : 3000;
+      path: __dirname,
+      filename: 'static/webpack-stats.json',
+      indent: true
+    }),
+  cssExtractTextPlugin = new ExtractTextPlugin({
+      filename: '[name]-[hash].css',
+      allChunks: true
+    }
+  ),
+  vendorPlugin = new webpack.optimize.CommonsChunkPlugin({
+    name: 'vendor',
+    filename: 'vendor.js'
+  })
 
 if (process.env.NODE_ENV === 'production') {
-    appEntry = [path.join(__dirname, 'client/index.js')];
-    devtool = 'source-map';
-    publicPath = '/static/bundles/';
-    plugins = [
-        stats,
-        new webpack.optimize.OccurrenceOrderPlugin(),
-        new webpack.optimize.CommonsChunkPlugin({name: 'vendor', filename: 'vendor.js'}),
-        new webpack.DefinePlugin({
-            'process.env': {
-                NODE_ENV: JSON.stringify('production')
-            }
-        }),
-        new ExtractTextPlugin({
-            filename: '[name]-[hash].css',
-            allChunks: true
-          }
-        ),
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                warnings: false
-            }
-        }),
-    ];
+  appEntry = [path.join(__dirname, 'client/index.js')]
+  devtool = 'source-map'
+  publicPath = '/static/bundles/'
+  plugins = [
+    statsPlugin,
+    vendorPlugin,
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+    cssExtractTextPlugin,
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
+  ]
 } else {
-    appEntry = [
+  appEntry = [
     // activate HMR for React
     'react-hot-loader/patch',
     `webpack-dev-server/client?http://localhost:${devServerPort}`,
     'webpack/hot/only-dev-server',
     './client/index.js'
-    ];
-    publicPath = `http://localhost:${devServerPort}/assets/bundles/`; // Tell django to use this URL to load packages and not use STATIC_URL + bundle_name
-    devtool = 'eval';
-    plugins = [
-        stats,
-        new webpack.optimize.CommonsChunkPlugin({name: 'vendor', filename: 'vendor.js'}),
-        new ExtractTextPlugin({
-                filename: '[name]-[hash].css',
-                allChunks: true
-              }
-        ),
-        new webpack.NoEmitOnErrorsPlugin(),
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NamedModulesPlugin(),
-        new webpack.DefinePlugin({
-            __DEV__: true
-        }),
-    ];
+  ]
+  publicPath = `http://localhost:${devServerPort}/assets/bundles/` // Tell django to use this URL to load packages and not use STATIC_URL + bundle_name
+  devtool = 'eval'
+  plugins = [
+    statsPlugin,
+    vendorPlugin,
+    cssExtractTextPlugin,
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new webpack.DefinePlugin({
+      __DEV__: true
+    })
+  ]
 }
 
-const buildPath = path.join(__dirname, 'static', 'bundles');
 
-
-module.exports = {
+export default {
   entry: {
     app: appEntry,
     vendor: ['react', 'react-dom', 'react-relay', 'react-router']
   },
   output: {
     path: buildPath,
-    filename: "[name]-[hash].js",
+    filename: '[name]-[hash].js',
     publicPath: publicPath
   },
   devtool,
@@ -95,23 +89,22 @@ module.exports = {
     hot: true,
     port: devServerPort,
     historyApiFallback: true,
-    stats: "errors-only",
+    stats: 'errors-only',
     contentBase: buildPath,
     headers: {
-      "Access-Control-Allow-Origin": "*"
+      'Access-Control-Allow-Origin': '*'
     },
     publicPath
   },
   module: {
-
-        rules: [{
-            test: /\.jsx?$/,
-            use: 'babel-loader',
-            exclude: /node_modules/
-        }, {
+    rules: [{
+      test: /\.jsx?$/,
+      use: 'babel-loader',
+      exclude: /node_modules/
+    }, {
       test: /\.css$/,
       use: ExtractTextPlugin.extract({
-        fallback: "style-loader",
+        fallback: 'style-loader',
         use: {
           loader: 'css-loader',
           options: {
@@ -123,7 +116,7 @@ module.exports = {
       test: /\.scss$/,
       include: [path.join(__dirname, 'node_modules'), path.join(__dirname, 'client', 'styles')],
       use: ExtractTextPlugin.extract({
-        fallback: "style-loader",
+        fallback: 'style-loader',
         use: [
           'css-loader',
           {
@@ -132,9 +125,9 @@ module.exports = {
               includePaths: [
                 path.resolve(__dirname, 'node_modules'),
                 path.join(__dirname, 'client', 'styles')
-              ],
+              ]
             }
-          },
+          }
         ]
       })
     },
@@ -143,21 +136,22 @@ module.exports = {
         exclude: [path.join(__dirname, 'node_modules'), path.join(__dirname, 'client', 'styles')],
         include: path.join(__dirname, 'client'),
         use: ExtractTextPlugin.extract({
-          fallback: "style-loader",
+          fallback: 'style-loader',
           use: [{
             loader: 'css-loader',
             options: {
               modules: true
-            }},
+            }
+          },
             {
               loader: 'sass-loader',
               options: {
                 includePaths: [
                   path.resolve(__dirname, 'node_modules'),
                   path.join(__dirname, 'client', 'styles')
-                ],
+                ]
               }
-            },
+            }
           ]
         })
       }, {
@@ -167,7 +161,7 @@ module.exports = {
             loader: 'url-loader',
             options: {
               limit: 1000,
-              name: "assets/[hash].[ext]"
+              name: 'assets/[hash].[ext]'
             }
           }
         ]
@@ -177,8 +171,8 @@ module.exports = {
     alias: {
       components: path.resolve(__dirname, 'client/components'),
       modules: path.resolve(__dirname, 'client/modules'),
-      utils: path.resolve(__dirname, 'client/utils'),
+      utils: path.resolve(__dirname, 'client/utils')
     }
   },
   plugins
-};
+}
