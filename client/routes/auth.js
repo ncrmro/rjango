cb6a3f3a977918395fbee1d7dcd6dfdc4e9b034e
ncrmro <ncrmro@gmail.com>
2017-10-25 07:12:21 -0500

    Add bundle support to auth routes

--- client/routes/auth.js
+++ client/routes/auth.js
@@ -1,16 +1,24 @@
-import Auth from 'modules/auth/Auth';
 import { authenticatedRoute } from 'modules/auth/utils'
+import Bundle from 'utils/bundleLoader'
+
+const Auth = props =>
+  <Bundle
+    load={() =>
+      import(/* webpackChunkName: "auth" */ 'modules/auth/Auth')}
+  >
+    { ViewerOrders => <ViewerOrders { ...props}/> }
+  </Bundle>
 
 const authRoutes = [
   {
     path: '/signup',
-    component: authenticatedRoute(Auth, false),
+    component: authenticatedRoute(Auth, false)
   },
   {
     path: '/login',
-    component: authenticatedRoute(Auth, false),
+    component: authenticatedRoute(Auth, false)
   }
-];
+]
 
 
-export default authRoutes;
+export default authRoutes
