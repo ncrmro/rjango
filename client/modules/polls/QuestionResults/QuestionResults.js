067f8b0c5884c1e2e46bfd4aa799a2b16a62a356
ncrmro <ncrmro@gmail.com>
2017-10-25 08:35:51 -0500

    Center the question result a bit more

--- client/modules/polls/QuestionResults/QuestionResults.js
+++ client/modules/polls/QuestionResults/QuestionResults.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { createFragmentContainer, graphql } from 'react-relay'
-
+import styles from './QuestionResults.scss'
 
 import {
   Bar,
@@ -23,23 +23,27 @@ const getChartDataFromRelayEdges = (edges) => {
 }
 
 export let QuestionResults = (props) =>
-  <div>
-    <h3>Results: </h3>
-    <h2>{props.question.questionText}</h2>
+  <div className={styles.root} >
+    <div>
+      <h3>Results: </h3>
+      <h2>{props.question.questionText}</h2>
 
-    <BarChart
-      width={600}
-      height={300}
-      data={getChartDataFromRelayEdges(props.question.choiceSet.edges)}
-      margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
-    >
-      <XAxis dataKey="choiceText" />
-      <YAxis dataKey="voteCount" />
-      <CartesianGrid  />
-      <Tooltip/>
-      <Legend />
-      <Bar dataKey="voteCount" fill="#8884d8" />
-    </BarChart>
+      <div className={styles.chart}>
+        <BarChart
+          // 90% screen width
+          width={window.screen.width * .9}
+          height={300}
+          data={getChartDataFromRelayEdges(props.question.choiceSet.edges)}
+        >
+          <XAxis dataKey="choiceText" />
+          <YAxis dataKey="voteCount" />
+          <CartesianGrid  />
+          <Tooltip/>
+          <Legend />
+          <Bar dataKey="voteCount" fill="#8884d8" />
+        </BarChart>
+      </div>
+    </div>
   </div>
 
 export default createFragmentContainer(QuestionResults, {
