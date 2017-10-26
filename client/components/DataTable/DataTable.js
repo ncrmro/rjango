844639e11e3fb6ad07dcb18820a8a94c6d1bc9ac
ncrmro <ncrmro@gmail.com>
2017-10-26 13:48:06 -0500

    Fix flow config

--- client/components/DataTable/DataTable.js
+++ client/components/DataTable/DataTable.js
@@ -2,42 +2,45 @@
 import React from 'react'
 import './DataTable.scss'
 
-export const DataCell = (props) =>
+type DataCellPropsType = {
+  column: {
+    name: string
+  },
+  row: Object,
+}
+
+export const DataCell = (props: DataCellPropsType) =>
   <td key={props.column.name} >
     {props.row[props.column.name]}
   </td>
 
-type DataTablePropsType = {
+export type DataTablePropsType = {
   columns: Array<{ name: string, label: string }>,
-  rows: Array<Object>,
+  rows: ?Array<Object>,
   children: ?Object,
 }
-export class DataTable extends React.Component {
-  props: DataTablePropsType
-
-  render() {
-    return (
-      <table >
-        <thead>
-        <tr>
-          {this.props.columns.map(column => <th
-            key={column.name} > {column.label}</th>)}
-        </tr>
-        </thead>
-        <tbody>
-        {this.props.children ? this.props.children : this.props.rows.map(row =>
-          <tr
-            key={row.id}
-          >{this.props.columns.map(column =>
-            <DataCell
-              column={column}
-              row={row}
-            />)}
-          </tr>)}
-        </tbody>
-      </table>
-    )
-  }
-}
+const DataTable = (props: DataTablePropsType) =>
+  <table >
+    <thead>
+    <tr>
+      {props.columns.map(column => <th
+        key={column.name} > {column.label}</th>)}
+    </tr>
+    </thead>
+    <tbody>
+    {props.children ? props.children : null}
+    {props.rows ? props.rows.map(row =>
+      <tr
+        key={row.id}
+      >{props.columns.map(column =>
+        <DataCell
+          column={column}
+          row={row}
+        />
+      )}
+      </tr>
+    ) : null}
+    </tbody>
+  </table>
 
 export default DataTable
--- client/components/DataTable/DataTableWithToolbar.js
+++ client/components/DataTable/DataTableWithToolbar.js
@@ -1,38 +1,30 @@
-// @flow
 import React from 'react'
 import DataTable from './DataTable'
 import Fab from 'react-mdc-web/lib/Fab/Fab'
 import Icon from 'react-mdc-web/lib/Icon/Icon'
 import styles from './DataTableWithToolbar.scss'
+import type DataTablePropsType from './DataTable'
 
-
-type DataTableWithToolbarProps = {
+type DataTableWithToolbarProps = DataTablePropsType & {
   addFunction: Function
 }
-export class DataTableWithToolbar extends React.Component {
-  props: DataTableWithToolbarProps
+const DataTableWithToolbar = (props: DataTableWithToolbarProps) =>
+  <div className={styles.root} >
+    <div
+      className={styles.dataTable}
+    >
+      <Fab
+        className={styles.addButton}
+        onClick={() => props.addFunction()}
+      >
+        <Icon name='add' />
+      </Fab>
+      <DataTable
+        {...props}
+        className={styles.dataTable}
+      />
+    </div>
 
-  render() {
-    return (
-      <div className={styles.root} >
-        <div
-          className={styles.dataTable}
-        >
-          <Fab
-            className={styles.addButton}
-            onClick={() => this.props.addFunction()}
-          >
-            <Icon name='add' />
-          </Fab>
-          <DataTable
-            {...this.props}
-            className={styles.dataTable}
-          />
-        </div>
-
-      </div>
-    )
-  }
-}
+  </div>
 
 export default DataTableWithToolbar
